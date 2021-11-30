9de75986eaa99cde48fef10b0d3d484c931b45f3
avi12 <avi6106@gmail.com>
2021-11-30 20:04:23 +0200

    Rollup config: - Updated JavaScript processing - Added HTML processing

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,5 @@
 import { terser } from "rollup-plugin-terser";
+import html from "@web/rollup-plugin-html";
 
 const isProduction = !process.env.ROLLUP_WATCH;
 
@@ -6,14 +7,26 @@ function createConfig(filename) {
   return {
     input: `src/${filename}.js`,
     output: {
-      format: "esm",
+      format: "cjs",
       file: `dist/build/${filename}.js`
     },
     plugins: [isProduction && terser()],
-    watch: {
-      clearScreen: false
-    }
+    watch: { clearScreen: true }
   };
 }
 
-export default [createConfig("skillshare-content-script-initialize")];
+function createOptionsPageConfig(dirOptions) {
+  return {
+    input: `src/${dirOptions}/options.html`,
+    output: {
+      dir: `dist/build/${dirOptions}`
+    },
+    plugins: [html({ minify: true })],
+    watch: { clearScreen: true }
+  };
+}
+
+export default [
+  createConfig("scripts/skillshare-content-script-initialize"),
+  createOptionsPageConfig("options")
+];
