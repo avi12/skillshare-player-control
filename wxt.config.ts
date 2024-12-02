a7627df82851dd3d99b4ce49d0b846075496d243
avi12 <avi6106@gmail.com>
2024-12-02 20:34:28 +0200

    Fixed wxt.config.ts

--- wxt.config.ts
+++ wxt.config.ts
@@ -23,7 +23,7 @@ export default defineConfig({
         ...manifest,
         browser_specific_settings: {
           gecko: {
-            id: "skillshare_player_controller@gmail.com"
+            id: "Skillshare_player_controller@gmail.com"
           }
         },
         developer: {
@@ -31,13 +31,17 @@ export default defineConfig({
           url
         }
       };
-    } else { // if not Firefox
+    } else {
+      // if not Firefox
       manifest.offline_enabled = true;
     }
     return manifest;
   },
   outDir: "build",
+  outDirTemplate: "{{browser}}-mv{{manifestVersion}}-{{mode}}",
   zip: {
-    excludeSources: ["*.env", ".env*"]
+    excludeSources: ["*.env", ".env*"],
+    artifactTemplate: "{{name}}-{{version}}-{{browser}}.zip",
+    sourcesTemplate: "{{name}}-{{version}}-{{browser}}-source.zip"
   }
 });
