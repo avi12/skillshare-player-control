7a6e83b84a976224ba9f92a265abdcd201e5dc15
Avi <avi6106@gmail.com>
2026-03-08 15:29:25 +0200

    Simplified wxt.config.ts

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig, type UserManifest } from "wxt";
+import { defineConfig } from "wxt";
 import packageJson from "./package.json" assert { type: "json" };
 
 // See https://wxt.dev/api/config.html
@@ -7,16 +7,14 @@ export default defineConfig({
   manifest({ browser }) {
     const url = packageJson.repository;
     const [, author, email] = packageJson.author.match(/(.+) <(.+)>/)!;
-    let manifest: UserManifest = {
+
+    return {
       name: "Skillshare Player Control",
       description: "Basic keyboard controls for the Skillshare player.",
       homepage_url: url,
-      // @ts-expect-error https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/manifest.json/author
-      author: browser === "opera" || browser === "firefox" ? packageJson.author : { email }
-    };
-    if (browser === "firefox") {
-      manifest = {
-        ...manifest,
+      author: browser === "opera" || browser === "firefox" ? packageJson.author : { email },
+      ...(browser !== "firefox" && { offline_enabled: true }),
+      ...(browser === "firefox" && {
         browser_specific_settings: {
           gecko: {
             id: "Skillshare_player_controller@gmail.com"
@@ -26,11 +24,8 @@ export default defineConfig({
           name: author,
           url
         }
-      };
-    } else {
-      manifest.offline_enabled = true;
-    }
-    return manifest;
+      })
+    };
   },
   outDir: "build",
   outDirTemplate: "{{browser}}-mv{{manifestVersion}}-{{mode}}",
