1144244fca8d683d81281fbc909177cc07dd2d5e
Avi <avi6106@gmail.com>
2026-06-16 11:57:41 +0300

    Restructured wxt.config.ts

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,32 +1,29 @@
 import { defineConfig } from "wxt";
-import packageJson from "./package.json" assert { type: "json" };
+import packageJson from "./package.json" with { type: "json" };
+
+const url = packageJson.repository;
+const [, author, email] = packageJson.author.match(/(.+) <(.+)>/)!;
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   srcDir: "src",
-  manifest({ browser }) {
-    const url = packageJson.repository;
-    const [, author, email] = packageJson.author.match(/(.+) <(.+)>/)!;
-
-    return {
-      name: "Skillshare Player Control",
-      description: "Basic keyboard controls for the Skillshare player.",
-      homepage_url: url,
-      author: browser === "opera" || browser === "firefox" ? packageJson.author : { email },
-      ...(browser !== "firefox" && { offline_enabled: true }),
-      ...(browser === "firefox" && {
-        browser_specific_settings: {
-          gecko: {
-            id: "Skillshare_player_controller@gmail.com"
-          }
-        },
-        developer: {
-          name: author,
-          url
+  manifest: ({ browser }) => ({
+    name: "Skillshare Player Control",
+    description: "Basic keyboard controls for the Skillshare player.",
+    homepage_url: url,
+    author: browser === "opera" || browser === "firefox" ? packageJson.author : { email },
+    ...(browser !== "firefox" && { offline_enabled: true }),
+    ...(browser === "firefox" && {
+      browser_specific_settings: {
+        gecko: {
+          id: "Skillshare_player_controller@gmail.com"
         }
-      })
-    };
-  },
+      },
+      developer: {
+        name: author,
+        url
+    }})
+  }),
   outDir: "build",
   outDirTemplate: "{{browser}}-mv{{manifestVersion}}-{{mode}}",
   zip: {
